266a118fa02ac4e2181ad66dabc89caf546dc247
kishore7403 <48860055+kishore7403@users.noreply.github.com>
2021-11-05 20:55:44 +0530

    0

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import tw from "tailwind-styled-components"
-import Map from "./components/Map.js"
+import Map from "./components/Map"
 import Link from "next/link"
 
 export default function Home(){
