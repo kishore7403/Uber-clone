4c9de5aef94874f1b52c45776f3b925e8f27ecaf
kishore7403 <48860055+kishore7403@users.noreply.github.com>
2021-11-05 21:25:55 +0530

    lat and long changed

--- pages/components/map.js
+++ pages/components/map.js
@@ -11,9 +11,9 @@ const Map=()=>{
         useEffect(() => {
           const map = new mapboxgl.Map({
           container: "map",
-          style: 'mapbox://styles/mapbox/streets-v11',
-          center: [80.270721,13.082680],
-          zoom: 10
+          style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
+          center: [-99.29011,39.39172],
+          zoom: 3
           });
           });
     return (
